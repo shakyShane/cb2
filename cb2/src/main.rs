3e5f6d4abb4228b26c698d1f4994aa08eb6b2d2f
Shane Osbourne <shane.osbourne8@gmail.com>
2019-01-16 09:27:33 +0000

    POC for killing multiple child processes

--- cb2/src/main.rs
+++ cb2/src/main.rs
@@ -21,11 +21,12 @@ use std::process::Stdio;
 use shared_child::SharedChild;
 use std::sync::Arc;
 use std::process::ExitStatus;
+use futures::sync::oneshot::{Sender, Receiver};
 
 struct Task {
     id: String,
     cmd: String,
-    parent: String
+    parent: String,
 }
 
 struct TaskParent {
@@ -39,10 +40,11 @@ impl TaskParent {
     }
 }
 
-impl Runnable for Task {
-    fn run(&self) -> (Box<Future<Item=Result<ExitStatus, ExitStatus>, Error=()>>, Box<Fn() -> ()>) {
+fn task (cmd: String, kill_recv: Receiver<String>) -> impl Future<Item=Result<ExitStatus, ExitStatus>, Error=()> {
+    box lazy(move || {
+
         let (tx, rx) = oneshot::channel();
-        let cloned_cmd = self.cmd.clone();
+        let cloned_cmd = cmd.clone();
 
         let mut child_process = Command::new("sh");
         child_process.arg("-c").arg(cloned_cmd);
@@ -61,12 +63,15 @@ impl Runnable for Task {
                     if s.success() {
                         tx.send(Ok(s))
                             .map_err(|e| {
-                                println!("err");
+                                match e {
+                                    Ok(s) => println!("Sent status code = {:?}", s),
+                                    Err(e) => println!("Error sending status code = {:?}", e)
+                                };
                             })
                     } else {
-                        tx.send(Err(s))
+                        tx.send(Ok(s))
                             .map_err(|e| {
-                                println!("err");
+                                println!("Error sending err = {:?}", e);
                             })
                     }
                 }
@@ -74,52 +79,77 @@ impl Runnable for Task {
             }
         }));
 
-        let teardown = move || {
-            clone_2.clone().kill();
-        };
+        tokio::spawn(lazy(move || {
+            kill_recv
+                .map(move |v: _| {
+                    clone_2.kill().expect("killed!");
+                    ()
+                })
+                .map_err(|e: _| {
+                    ()
+                })
+        }));
 
-        (box rx.map_err(|e| { println!("got error"); }), box teardown)
-    }
+        rx.map_err(|e| { println!("got error"); })
+    })
 }
 
-trait Runnable {
-    fn run(&self) -> (Box<Future<Item=Result<ExitStatus, ExitStatus>, Error=()>>, Box<Fn() -> ()>);
-}
 
 fn main() {
     tokio::run(lazy(|| {
-        let parent = TaskParent::new();
+//        let (s, r) = unbounded();
 
-        let t = Task{
-            id: "01".to_string(),
-            cmd: "echo 'start' && sleep 2 && echo 'end'".to_string(),
-            parent: parent.id.clone(),
-        };
+        tokio::spawn(lazy(move || {
+            let parent = TaskParent::new();
+            let (kill_msg_sender, kill_msg_recv) = oneshot::channel();
+            let (kill_msg_sender_2, kill_msg_recv_2) = oneshot::channel();
+            let t = task("echo 'start 1' && sleep 3 && echo 'end 1'".to_string(), kill_msg_recv);
+            let t2 = task("echo 'start 2' && sleep 3 && echo 'end 2'".to_string(), kill_msg_recv_2);
+            let items = vec![t, t2];
 
-        let t2 = Task{
-            id: "02".to_string(),
-            cmd: "echo 'start' && sleep 2 && echo 'end'".to_string(),
-            parent: parent.id.clone(),
-        };
+            tokio::spawn(lazy(move || {
+                thread::sleep(Duration::from_secs(1));
+                futures::collect(
+                    vec![
+                        kill_msg_sender.send("DIE!".to_string()),
+                        kill_msg_sender_2.send("DIE!".to_string()),
+                    ]
+                )
+                    .map(|v| ())
+                    .map_err(|v| ())
+            }));
 
-        let mut tear_downs = vec![];
-        let (rx, teardown) = t.run();
-
-        tear_downs.push(teardown);
-        let (rx2, teardown2) = t2.run();
-        tear_downs.push(teardown2);
-
-        println!("Got return values");
-
-        rx.join(rx2).map(|(v1, v2)| {
-            println!("SUCCESS VALUE={:?}", v1);
-            println!("SUCCESS VALUE={:?}", v2);
-        }).map_err(|e| {
-            println!("ERROR VALUE={:?}", e)
-        })
-        .wait();
-
+            futures::collect(items)
+                .map(|v| {
+                    println!("value-received={:?}", v);
+                    ()
+                })
+                .map_err(|_: ()| {
+                    println!("e=");
+                    ()
+                })
+        }));
 
         Ok(())
+//        tds.into_iter().for_each(|td| {
+//            td();
+//        });
+//        let mut tear_downs = vec![];
+//        let (rx, teardown) = t.run();
+//
+//        tear_downs.push(teardown);
+//        let (rx2, teardown2) = t2.run();
+//        tear_downs.push(teardown2);
+//
+//        println!("Got return values");
+//
+//        rx.join(rx2).map(|(v1, v2)| {
+//            println!("SUCCESS VALUE={:?}", v1);
+//            println!("SUCCESS VALUE={:?}", v2);
+//        }).map_err(|e| {
+//            println!("ERROR VALUE={:?}", e)
+//        })
+//        .wait();
+//        Ok(())
     }))
 }
